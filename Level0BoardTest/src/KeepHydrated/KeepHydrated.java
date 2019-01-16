0ef74d4eeef84119118605d1d8f3eb1a431251d1
peter <30840431+peterfregoso@users.noreply.github.com>
2019-01-15 18:27:20 -0800

    Add files via upload

---
+++ Level0BoardTest/src/KeepHydrated/KeepHydrated.java
@@ -0,0 +1,27 @@
+package KeepHydrated;
+
+public class KeepHydrated {
+	/**
+	 * Nathan loves cycling.
+	 * 
+	 * Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres
+	 * of water per hour of cycling.
+	 * 
+	 * You get given the time in hours and you need to return the number of litres
+	 * Nathan will drink, rounded to the smallest value.
+	 * <pre>
+	 * For example:
+	 * 
+	 * time = 3 ----> litres = 1
+	 * 
+	 * time = 6.7---> litres = 3
+	 * 
+	 * time = 11.8--> litres = 5
+	 * </pre>
+	 **/
+	public int Liters(double time) {
+		// TODO Write your method here
+		return 0;
+	}
+
+}
